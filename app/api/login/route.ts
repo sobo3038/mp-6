6e12e0e7ea08270f9c082e5badd03bb1ff568a1f
Sofia Boada <sofiabl@bu.edu>
2025-04-27 23:07:54 -0400

    mp-6 working locally

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,7 +1,7 @@
 // app/api/login/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() { 
     const clientId = process.env.GITHUB_CLIENT_ID!;
     const redirectUri = process.env.GITHUB_REDIRECT_URI!;
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user`;
